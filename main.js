168ec804d3aa830318ea0e8e029b5a6421c8336e
marcossantos1 <marcossantosmsms604@gmail.com>
2022-02-09 14:58:32 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,41 @@
+ var seg = 0;
+  var min = 0;
+  var hor = 0;
+  var intervalo;
+
+  function prime(digito){
+    if(digito<10){
+        return("0"+digito);
+    }else{
+        return(digito);
+    }
+  }
+  
+  function inicia(){
+     time();
+     intervalo=setInterval(time,1000);
+  }
+
+  function pausar(){
+     clearInterval(intervalo);
+  }
+
+  function reiniciar(){
+       clearInterval(intervalo);
+       seg=0;
+       min=0;
+       document.getElementById("time").innerHTML="00:00:00";
+  }
+ 
+   function time(){
+    seg++;
+    if(seg==60){
+       min++;
+       seg=0;
+      if(min==60){
+         hor++;
+         min=0;
+      }
+    }
+    document.getElementById("time").innerText=prime(hor)+":"+prime(min)+":"+prime(seg);
+   }
